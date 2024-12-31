dd0384106ba5dd4bb11f46be06ad2bafdb60382e
Viraat Das <viraat.laldas@gmail.com>
2024-12-31 10:56:29 -0800

    Auto commit - bulk update

---
+++ Humanize/Humanize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Humanize
+//
+//  Created by Das, Viraat on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Humanize/Humanize/HumanizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanizeApp.swift
+//  Humanize
+//
+//  Created by Das, Viraat on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HumanizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
